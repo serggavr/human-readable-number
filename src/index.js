fc2d78ab63386469e7dd2e6f764af3ed9d582d90
g.sergey@bk.ru <example@gmail.com>
2021-08-16 14:00:38 +0300

    fix: error

--- src/index.js
+++ src/index.js
@@ -1,3 +1,64 @@
 module.exports = function toReadable (number) {
-  
-}
+    if (num == 0) {
+        return 'zero'
+    };
+    const dictToNine = {
+        1: "one",
+        2: "two",
+        3: "three",
+        4: "four",
+        5: "five",
+        6: "six",
+        7: "seven",
+        8: "eight",
+        9: "nine"
+    };
+    const dictToNineteen = {
+        10: "ten",
+        11: "eleven",
+        12: "twelve",
+        13: "thirteen",
+        14: "fourteen",
+        15: "fifteen",
+        16: "sixteen",
+        17: "seventeen",
+        18: "eighteen",
+        19: "nineteen"
+    };
+    const dictToNinety = {
+        20: "twenty",
+        30: "thirty",
+        40: "forty",
+        50: "fifty",
+        60: "sixty",
+        70: "seventy",
+        80: "eighty",
+        90: "ninety"
+    };
+    let answer = '';
+    let upRest;
+    let downRest;
+    let numString = num.toString();
+    switch (numString.length) {
+        case 3:
+            downRest = num % 100;
+            upRest = (num - downRest) / 100;
+            answer += `${dictToNine[upRest]} hundred `;
+            num -= (upRest * 100);
+        case 2:
+            if (num < 20) {
+                answer += `${dictToNineteen[num]}`;
+            }
+            if (num % 10 == 0) {
+                answer += `${dictToNinety[num]}`;
+            } else {
+                downRest = num % 10;
+                upRest = num - downRest;
+                answer += `${dictToNinety[upRest]} `;
+                num -= upRest;
+            };
+        case 1:
+            answer += `${dictToNine[downRest]}`;
+    }
+    return answer;
+};
